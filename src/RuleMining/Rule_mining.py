a81c75628172680bd9a23b0727e7e69f28310736
Max Luttmann <max.luttmann@stud.uni-hannover.de>
2025-10-15 16:33:29 +0200

    State of work 15.10 code based on VANILLA removed symbolic predictions predicate mappings are saved as json there are also some smaller changes + refactoring
    added RuleMining.py so far containing code for generating pos/neg examples for the rule mining algorithm

---
+++ src/RuleMining/Rule_mining.py
@@ -0,0 +1,159 @@
+from rdflib import Graph, URIRef, BNode, Literal, Namespace
+import json
+import numpy as np
+import warnings
+
+
+
+def mine_rules(transformed_kg:Graph, targets:set, transform_output_dir:str, ontology_path:str, prefix:str, max_depth:int=3, set_size:int=100,
+        type_predicate:str="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"):
+    """
+    Mines rules for all original predicates of a normalized knowledge graph.
+    
+    Iterates over the original predicates, forming genereation and validation set for each.
+    Then calls rule mining method for each predicate.
+
+    Args:
+        transformed_kg -- knowledge graph to mine rules for
+        targets -- list of target predicates from the original kg to mine for
+        transform_output_dir -- directory of normalization/validation result containing predicate mappings
+        ontology_path -- path to given ontology
+        prefix -- prefix
+        max_depth -- max length of paths in graph corresponding to rule length
+        set_size -- number of elements in G and V
+
+    Returns:
+        no return
+        (but: produces a .csv file containing the mined rules)
+    """
+
+    print(f"using {type_predicate} as type predicate.\n")
+
+    # load predicate mappings
+    with open(f"{transform_output_dir}/predicate_mappings.json", "r", encoding="utf-8") as p_map_file:
+        predicate_mappings = json.load(p_map_file)
+    with open(f"{transform_output_dir}/no_predicate_mappings.json", "r", encoding="utf-8") as np_map_file:
+        neg_predicate_mappings = json.load(np_map_file)
+
+    result = [[]]
+
+
+
+    for p in targets:
+        print(f"creating input sets G and V for target predicate {p}...\n")
+
+        # getting post normalization instances of target predicate and the negative instances from validation
+        predicates = [k for k, v in predicate_mappings.items() if v == p]
+        neg_predicates = [k for k, v in neg_predicate_mappings.items() if v in predicates]
+
+
+
+        # create positive examples
+        filter_p = "||".join(f"?p = <{pred}>" for pred in predicates)
+
+        #TODO randomization needed but order by rand is too expensive
+        query_g = f""" SELECT ?s ?o
+                    WHERE {{
+                    ?s ?p ?o .
+                    FILTER ({filter_p})
+                    }}
+                    LIMIT {set_size}"""
+
+        g = []
+        for row in transformed_kg.query(query_g):
+            g.append(row)
+            
+        len_g = len(g)
+        if len_g < set_size:
+            warnings.warn(f"There aren't enough positive examples in the graph, proceeding with {len_g} examples.\n", UserWarning)        #create negative examples
+
+
+
+        # TODO: maybe enable custom input for extra negative examples
+        #create negative examples
+        # first, get all constraint violating pairs
+
+        filter_np = "||".join(f"?p = <{pred}>" for pred in neg_predicates)
+
+        v = []
+        if filter_np:
+            query_v = f""" SELECT ?s ?o
+                        WHERE {{
+                        ?s ?p ?o .
+                        FILTER ({filter_np})
+                        }}
+                        LIMIT {set_size}"""
+        
+            for row in transformed_kg.query(query_v):
+                v.append(row)
+
+        len_v = len(v)
+        if len_v < set_size:
+            warnings.warn(f"{len_v} examples found from constraint violations, selecting remaining {set_size - len_v} examples from graph.\n", UserWarning)   
+            if filter_np:
+                not_filter = f"{filter_np} || {filter_p}"
+                not_filter = not_filter.replace("=", "!=")
+            else: 
+                not_filter = filter_p.replace("=", "!=")
+            # TODO if not enough add more random v-entries, maybe: allow custom input 
+            query_v_fill = f""" SELECT ?s ?o
+                            WHERE {{
+                            ?s ?p ?o .
+                            FILTER ({not_filter})
+                            }}
+                            LIMIT {set_size - len_v}"""
+            
+            for row in transformed_kg.query(query_v_fill):
+                v.append(row)
+
+        len_v = len(v)
+        if len_v < set_size:
+            warnings.warn(f"There aren't enough negative examples in the graph, proceeding with {len_v} examples.\n", UserWarning)   
+
+        print("---------------------\n")
+
+        #print(f"mining rules for target predicate {p}...")
+        #result.extend(mine_rules_for_target_predicate(g, v, p, predicates, neg_predicates, transformed_kg, prefix, type_predicate, ontology_path, max_depth))
+        #TODO add result to csvs
+
+    # 
+    return
+
+def mine_rules_for_target_predicate(g:list, v:list, target:URIRef, predicates:list[str], neg_predicates:list[str],
+                                    transformed_kg:Graph, prefix:str, type_predicate:str, ontology_path:str,  max_depth:int=3):
+    """
+
+
+    Args:
+        G -- generation set
+        V -- validation set
+        predicates -- all post normalization versions of the original predicates rules are mined for
+        neg_predicates -- 
+        transformed_kg -- knowledge graph to mine rules for
+        ontology_path -- path to given ontology
+        prefix -- prefix
+        max_depth -- max length of paths in graph corresponding to rule length
+
+    Returns:
+        R_out -- mined rules for the target predicate
+    """
+
+    kg = transformed_kg
+    n = Namespace(prefix)
+    R_out = np.zeros()
+    frontiers = {}
+    candidates = {}
+    #TODO fill sets G and V
+
+
+
+
+    #TODO rudik
+    return 
+
+
+#TODO help function get_domain/range
+
+#TODO help function calc_heuristic; important to isolate for modularity
+
+#TODO help function expand_frontiers(list of current nodes)
---
+++ src/Transform.py
@@ -0,0 +1,172 @@
+###################
+#
+# this file is just used for developing, will not be part of final product
+#
+####################
+
+import numpy as np
+import json
+import pandas as pd
+from rdflib.plugins.sparql.processor import SPARQLResult
+from pandasql import sqldf
+from rdflib import Graph, URIRef
+import re
+import os
+import time
+from Normalization.Validation import travshacl
+from Normalization.Normalization_transform import transform
+from RuleMining.Rule_mining import mine_rules
+
+import logging
+
+def convert_nt_file_with_prefix(input_file, output_file, prefix):
+    with open(input_file, 'r', encoding='utf-8') as f_in, open(output_file, 'w', encoding='utf-8') as f_out:
+        for line in f_in:
+            if line.strip() == "":
+                continue  # Überspringe leere Zeilen
+
+            parts = line.split()
+
+            if len(parts) < 3:
+                print(f"Warnung: Ungültige Zeile gefunden (zu wenige Teile): {line}")
+                continue
+
+            # Präfix zu Subjekt hinzufügen
+            parts[0] = f"<{prefix}{parts[0]}>"
+
+            # Präfix zu Prädikat hinzufügen
+            cutoff = parts[1].find("_")
+            if cutoff > -1:
+                parts[1] = f"<{prefix}{parts[1][0:cutoff]}>"
+            else:
+                parts[1] = f"<{prefix}{parts[1]}>"
+            # Präfix zu Objekt hinzufügen, wenn es ein URI ist
+            parts[2] = f"<{prefix}{parts[2]}>"
+
+            # Zeile mit Punkt abschließen
+            if not parts[-1].endswith('.'):
+                parts[-1] += '.'
+
+            f_out.write(' '.join(parts) + '\n')
+
+
+
+def initialize(input_config):
+    """
+    Initializes the system using the configuration provided in the input file. It reads
+    the configuration file, extracts necessary parameters, constructs paths for various
+    files and folders, and logs the loaded configuration details.
+
+    Args:
+        input_config (str): The path to the configuration file in JSON format.
+
+    Returns:
+        tuple: A tuple containing the following elements in order:
+            - prefix (str): The prefix specified in the configuration file.
+            - rules (str): Path to the rules file.
+            - rdf (str): Path to the RDF file.
+            - path (str): Path to the knowledge graph (KG) directory.
+            - predictions_folder (str): Path to the predictions folder.
+            - constraints (str): Path to the constraints folder.
+            - kg (str): Name of the knowledge graph (KG).
+            - pca_threshold (float): PCA threshold value from the configuration file.
+    """
+    print(f"Reading configuration from {input_config}")
+    with open(input_config, "r") as input_file_descriptor:
+        input_data = json.load(input_file_descriptor)
+
+    prefix = input_data['prefix']
+    kg_name = input_data['KG']
+    kg_path = os.path.join('Data', 'KG', input_data['KG'])
+    rules_path = os.path.join('Data', 'Rules', input_data['rules_file'])
+    rdf_path = os.path.join(kg_path, input_data['rdf_file'])
+    ontology_path = os.path.join('Data', 'Ontology', input_data['ontology_file'])
+    predictions_folder = os.path.join('Data', 'Predictions', input_data['KG'] + "_predictions")
+    constraints_folder = os.path.join('Data', 'Constraints',input_data['constraints_folder'])
+    pca_threshold = input_data['pca_threshold']
+
+    print(f"Configuration loaded:\n"
+          f"- Prefix: {prefix}\n"
+          f"- Rules file: {rules_path}\n"
+          f"- RDF file: {rdf_path}\n"
+          f"- Predictions folder: {predictions_folder}\n"
+          f"- Constraints folder: {constraints_folder}\n"
+          f"- PCA Threshold: {pca_threshold}")
+
+    logger.info(f"Configuration loaded:\n "
+          f"- Prefix: {prefix}\n"
+          f"- Rules file: {rules_path}\n"
+          f"- RDF file: {rdf_path}\n"
+          f"- Predictions folder: {predictions_folder}\n"
+          f"- Constraints folder: {constraints_folder}\n"
+          f"- PCA Threshold: {pca_threshold}")
+
+    return prefix, rules_path, rdf_path, kg_path, ontology_path, predictions_folder, constraints_folder, kg_name, pca_threshold
+
+
+
+if __name__ == '__main__':
+    try:
+        #MAX check cwd
+        cwd = os.getcwd()
+        if (os.path.basename(cwd) != 'src'):
+            raise RuntimeError(f"Expected CWD to be the projects 'src' folder, but got {cwd}")
+
+        start_time = time.time()
+        print("Starting symbolic prediction generation...")
+
+        # Initialize configuration
+        input_config = './Data/input.json'
+
+        # Create logs directory if it doesn't exist
+        logs_dir = 'logs'
+        if not os.path.exists(logs_dir):
+            os.makedirs(logs_dir)
+
+        # Set up logging with file output and rotation
+        #MAX: whats rotation?
+        log_level = 'INFO'  # Default to INFO level
+        timestamp = time.strftime('%Y%m%d-%H%M%S')
+        log_file = os.path.join(logs_dir, f'symbolic_predictions_{timestamp}.log')
+
+        # Configure logging to write to both console and file
+        logging.basicConfig(
+            level=getattr(logging, log_level),
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            handlers=[
+                logging.FileHandler(log_file),
+                logging.StreamHandler()  # This sends output to console too
+            ]
+        )
+        logger = logging.getLogger(__name__)
+
+        #Initializaing from the input.json file
+        prefix, rules_path, rdf_path, kg_path, ontology_path, predictions_folder, constraints_folder, kg_name, pca_threshold = initialize(input_config)
+
+
+
+        g = Graph()
+        g.parse(rdf_path, format='nt')
+        predicates = ["<http://FrenchRoyalty.org/predecessor>", "<http://FrenchRoyalty.org/child>"]
+        filter_p = "||".join(f"?p != {pred}" for pred in predicates)
+        query = f""" SELECT ?s ?p ?o
+                    WHERE {{
+                    ?s ?p ?o .
+                    FILTER({filter_p})
+                    }}
+                    ORDER BY RAND()
+                    LIMIT 10"""
+
+        for row in g.query(query):
+            print(row)
+
+
+       
+    except Exception as e:
+        error_msg = f"Error occurred during execution: {str(e)}"
+        print(f"\n{error_msg}")
+        if 'logger' in locals():
+            logger.error(error_msg, exc_info=True)  # Logs the full traceback
+        raise
+
+     
